4c7856c95134e109a0c23437aedc8ea2f690e849
Riri Z <riri.zamor@gmail.com>
2020-06-25 01:21:28 +0200

    page home almost done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,24 @@ import React, { Component } from "react";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
+import "../styles/Home.scss";
 class Home extends Component {
   render() {
     return (
       <div className="home-container">
         <Header />
         <main className="home-main">
-          <h1>Join the best community of healthcare Practitians !</h1>
+          <h1 className="Welcome">
+            Join the best community of healthcare Practitians !
+          </h1>
         </main>
         <div className="Sub-home-container">
-          <Link to="/">
-            <Button outline>Create a free account</Button>
-          </Link>
-
+          <div className="Sub-home-container-button">
+            <Link to="/">
+              <Button outline>Create a free account</Button>
+            </Link>
+            <a href="toto">or login &rarr; </a>
+          </div>
           <p>
             Share your knowledge with your peers and find the answers to your
             questions. StackOverDoc ranks responses in order of relevance, using
