89a95a32d7f0e762af3b3d8cb9708be529b14154
Riri Z <riri.zamor@gmail.com>
2020-06-24 22:32:23 +0200

    dunno

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
