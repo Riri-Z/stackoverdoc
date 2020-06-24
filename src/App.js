b72b6329828f5815907319c65eaff38c3d1054f5
Riri Z <riri.zamor@gmail.com>
2020-06-24 22:16:57 +0200

    route + button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/Header";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-function App() {
-  return <div className="App">
-    <Header/>
-  </div>;
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
