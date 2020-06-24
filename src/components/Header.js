c72ebc873c17efcd2b4bf6a61245f69db84eb417
tmicka23 <mickathp42@gmail.com>
2020-06-25 01:15:54 +0200

    enable navbar for mobile & tablet views

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@ import logo from "../images/TitleLogo.png";
 import "../styles/Header.scss";
 
 function Header() {
+  const [navOpened, setNavOpened] = useState(true);
   return (
     <>
       <nav className='_sd-navbar'>
         <a className='_sd-navbar-brand' href='sof'>
           <img src={logo} alt='stackoverdoc' />
         </a>
-        <ul className='_sd-navbar-menu'>
+        <ul
+          className={navOpened ? "_sd-navbar-menu opened" : "_sd-navbar-menu"}>
           <li>
             <a href='fdhy' className='btn btn-primary sd-btn'>
               Sign Up
@@ -19,6 +21,13 @@ function Header() {
             <a href='fdhy'>Login</a>
           </li>
         </ul>
+        {navOpened && (
+          <div
+            className='sd-overlay'
+            onClick={() => setNavOpened(!navOpened)}
+          />
+        )}
+        <i className='icon-menu' onClick={() => setNavOpened(!navOpened)}></i>
       </nav>
     </>
   );
