cc4a6f4378a392c7e022a57e83e5d7ace046bf5c
tmicka23 <mickathp42@gmail.com>
2020-06-25 14:14:45 +0200

    finish profil page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from "react";
-<<<<<<< HEAD
-import "./App.css";
-import Login from "./pages/Login";
-
-function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  )
-=======
+import Profil from "./pages/Profil";
 import Header from "./components/Header";
-import "./styles/App.scss";
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Profil />
     </div>
   );
->>>>>>> 7e048027060171af2eceaa98ed1f8f034c5a8d9f
 }
 
 export default App;
