d53198867f8214f234c985afc88897b8cf5aa42b
Adama SONKO <thioff1@gmail.com>
2020-06-25 19:11:05 +0200

    responsive

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import FormLogin from "../components/formLogin.js";
+import picture from "../images/background2.png"
 import '../styles/login.scss'
 
 class Login extends React.Component{
     render() {
         return (
             <div className='login-page'>
-                <div className='form-container'>
-                    <div className='login-text'>
-                        <h3>Create an account!</h3>
-                        <p>Start to collaborate with us</p>
-                    </div>                    
-                </div>
-                <FormLogin/>             
+                <div className="log-container">
+                    <div className='form-container'>
+                        <div className='login-text'>
+                            <h3>Create an account!</h3>
+                            <p>Start to collaborate with us</p>
+                        </div>                    
+                    </div>
+                    <img src={picture} alt="doctor"/>
+                    <div className="large-screen">
+                        <FormLogin/>
+                    </div>
+                </div>                            
             </div>
             
         )
