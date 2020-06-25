1f3bfe6b23226f53018766f8d51bc9abc80cc28e
Riri Z <riri.zamor@gmail.com>
2020-06-25 13:20:35 +0200

    imput presque finalisé

--- src/pages/Question_page.js
+++ src/pages/Question_page.js
@@ -1,9 +1,20 @@
 import Header from "../components/Header";
-import React /* , { useState }  */ from "react";
+import React, { useState } from "react";
 import "../styles/Qpages.scss";
-import FormAskquestion from "../components/formAskquestion";
+import Tag from "../components/tag";
 
 const Qpages = () => {
+  const [tags, setTag] = useState([]);
+
+  const handleDelete = (id) => {
+    const updatedTags = [...tags];
+    const index = updatedTags.findIndex((tag) => tag.id === id);
+
+    updatedTags.splice(index, 1);
+
+    setTag(updatedTags);
+  };
+
   return (
     <>
       <div>
@@ -38,7 +49,26 @@ const Qpages = () => {
           </p>
         </div>
         <div className="AreaAnswer">
-          <FormAskquestion />
+          <form className="form-group AskFormContainer">
+            <label for="AskTitle" className="AskTitle">
+              Ask your question :
+            </label>
+            <textarea
+              className="form-control AskTextarea"
+              rows="3"
+              placeholder="I wonder if..."
+            ></textarea>
+            <div className="QuestionContainer">
+              <ul>
+                {tags.map((tag) => (
+                  <Tag key={tag.id} details={tag} onDelete={handleDelete} />
+                ))}
+              </ul>
+            </div>
+            <button type="submit" className="btn mb-2 AskSubmit">
+              <i className="icon-send"></i>
+            </button>
+          </form>
         </div>
       </div>
     </>
