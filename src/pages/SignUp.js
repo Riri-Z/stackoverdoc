ed321fea7c347977761f01f981fd85daf675d1b5
tmicka23 <mickathp42@gmail.com>
2020-06-25 23:19:15 +0200

    enable authentification

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,43 +1,41 @@
 import React, { useCallback } from "react";
 import { withRouter } from "react-router";
 import app from "../services/base";
-import FormLogin from "../components/formLogin";
-import picture from "../images/background2.png"
-import '../styles/signup.scss'
+import FormSignUp from "../components/FormSignUp";
+import picture from "../images/background2.png";
+import "../styles/signup.scss";
 
-const SignUp = ({ history }) =>{
+const SignUp = ({ history }) => {
+  const handleSignUp = useCallback(
+    async (event) => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        await app
+          .auth()
+          .createUserWithEmailAndPassword(email.value, password.value);
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
 
-  const handleSignUp = useCallback(async event => {
-    event.preventDefault();
-    const { email, password } = event.target.elements;
-    try {
-      await app
-        .auth()
-        .createUserWithEmailAndPassword(email.value, password.value);
-      history.push("/");
-    } catch (error) {
-      alert(error);
-    }
-  }, [history]);
-
- 
-    
-        return (
-            <div className='signup-page'>
-                <div className='form-container'>
-                    <div className='login-text'>
-                        <h3>Create an account!</h3>
-                        <p>Start to collaborate with us</p>
-                    </div>
-                    <img src={picture} alt="doctor"/>
-                    <div className="large-screen">
-                    <FormLogin onSubmit={handleSignUp}/>
-                    </div>
-                </div>
-                
-            </div>
-            
-        );
+  return (
+    <div className='signup-page'>
+      <div className='form-container'>
+        <div className='login-text'>
+          <h3>Create an account!</h3>
+          <p>Start to collaborate with us</p>
+        </div>
+        <img src={picture} alt='doctor' />
+        <div className='large-screen'>
+          <FormSignUp onSubmit={handleSignUp} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(SignUp);
