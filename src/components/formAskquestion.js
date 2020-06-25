c48c3ef2efbf8ee68546ee1893272ccb602ccafe
tmicka23 <mickathp42@gmail.com>
2020-06-25 11:02:55 +0200

    on bois l'apéro vendredi les copains ?

--- src/components/formAskquestion.js
+++ src/components/formAskquestion.js
@@ -1,48 +1,48 @@
-import React, { useState } from 'react';
-import '../styles/formAskQuestion.scss';
+import React, { useState } from "react";
+import "../styles/formAskQuestion.scss";
 import Tag from "./tag";
 import FormTag from "./formTag";
 
 const FormAskquestion = () => {
-
   const [tags, setTag] = useState([]);
 
-  const handleDelete = id => {
+  const handleDelete = (id) => {
     const updatedTags = [...tags];
-    const index = updatedTags.findIndex(tag => tag.id === id);
+    const index = updatedTags.findIndex((tag) => tag.id === id);
 
     updatedTags.splice(index, 1);
 
     setTag(updatedTags);
   };
 
-  const handleAdd = tag => {
+  const handleAdd = (tag) => {
     const updatedTags = [...tags];
-    updatedTags.push(tag);
 
+    if (tag.name.length > 0) {
+      updatedTags.push(tag);
+    }
     setTag(updatedTags);
   };
 
   return (
     <>
       <form>
-        <div className="form-group AskFormContainer">
-          <label for="AskTitle">Ask your question :</label>
-          <textarea className="form-control AskTextarea" rows="3" placeholder="I wonder if..."></textarea>
+        <div className='form-group AskFormContainer'>
+          <label for='AskTitle'>Ask your question :</label>
+          <textarea
+            className='form-control AskTextarea'
+            rows='3'
+            placeholder='I wonder if...'></textarea>
           <div>
             <ul>
-              {tags.map(tag => (
-                <Tag
-                  key={tag.id}
-                  details={tag}
-                  onDelete={handleDelete}
-                />
+              {tags.map((tag) => (
+                <Tag key={tag.id} details={tag} onDelete={handleDelete} />
               ))}
             </ul>
             <FormTag onTagAdd={handleAdd} />
           </div>
-          <div className="AskButtonContainer">
-            <button type="submit" className="btn mb-2 AskSubmit">
+          <div className='AskButtonContainer'>
+            <button type='submit' className='btn mb-2 AskSubmit'>
               <i className='icon-send'></i>
             </button>
           </div>
