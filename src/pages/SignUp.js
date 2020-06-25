4b8e143e1b0be67310fda14ff196735c64d9f2d1
Adama SONKO <thioff1@gmail.com>
2020-06-25 19:41:59 +0200

    responsive

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormLogin from "../components/formSignUp.js";
+import picture from "../images/background2.png"
 import '../styles/signup.scss'
 
 class Login extends React.Component{
@@ -11,7 +12,10 @@ class Login extends React.Component{
                         <h3>Create an account!</h3>
                         <p>Start to collaborate with us</p>
                     </div>
-                    <FormLogin/>
+                    <img src={picture} alt="doctor"/>
+                    <div className="large-screen">
+                        <FormLogin/>
+                    </div> 
                 </div>
                 
             </div>
