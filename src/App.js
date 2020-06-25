2e5d847e950431e84eb3e93f35020a441307fd6a
tmicka23 <mickathp42@gmail.com>
2020-06-25 15:08:32 +0200

    enable home background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Profil from "./pages/Profil";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <Profil />
+      <Home />
+      {/* <Profil /> */}
     </div>
   );
 }
